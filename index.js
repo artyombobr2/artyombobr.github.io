629086393ee2c27a2515866ed08559dde0728e68
artyombobr <artyombobr@yandex-team.ru>
2019-06-28 22:48:46 +0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    setInterval(()=> {
-       document.querySelector('.counter__number').innerHTML = 5.14 + new Date() / 10000000000000000;
+       document.querySelector('.counter__number').innerHTML = (5.14 + new Date() / 10000000000000000).toFixed(14);
    }, 100)
 });
